ac3a5ca7836658ce8b7c35410f474632f1b3e6bb
MichaelLuppino <michael.luppino01@gmail.com>
2023-02-11 00:30:40 +0100

    add Sushi images and card id

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import { Component } from "react";
 import Card from "./components/card";
 import Navbar from "./components/navbar";
 import california from './images/california.png';
+import dynamite from './images/dynamite.png';
+import philadelphia from './images/philadelphia.png';
+import rainbow from './images/rainbow.png';
 import dragon from './images/dragon.png';
+import shrimp from './images/shrimp.png';
 
 class App extends Component {
   state = {
     cards: [
       { id: 0, nome: 'California', prezzo: 1.99, immagine: california, quantità: 0 },
-      { id: 1, nome: 'Dragon', prezzo: 3.49, immagine: dragon, quantità: 0 }
+      { id: 1, nome: 'Dynamite', prezzo: 1.99, immagine: dynamite, quantità: 0 },
+      { id: 2, nome: 'Philadelphia', prezzo: 1.99, immagine: philadelphia, quantità: 0 },
+      { id: 4, nome: 'Dragon', prezzo: 3.49, immagine: dragon, quantità: 0 },
+      { id: 3, nome: 'Rainbow', prezzo: 1.99, immagine: rainbow, quantità: 0 },
+      { id: 5, nome: 'Shrimp', prezzo: 3.49, immagine: shrimp, quantità: 0 }
+     
     ]
   }
 
